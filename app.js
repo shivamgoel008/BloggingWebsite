070960c0d21942929982e47dd9e3d5c07d0745a6
Shivam Goel <55030452+shivamgoel008@users.noreply.github.com>
2021-03-20 18:42:16 +0530

    database

--- app.js
+++ app.js
@@ -3,6 +3,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+
+// for data base 
+const mongoose = require("mongoose");
+
+// for capital and small words 
 const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -10,23 +15,40 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+
 app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+
+// connecting mongoose with the server
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
 // declaring the global array for storing the data 
-let posts=[];
+// let posts=[];
 
 
     app.get("/",function(req,res){
-      res.render("home", {
 
-        // passing the key value pair to the ejs file 
-        // passing the global array to the ejs to print that array 
-        startingContent: homeStartingContent,
-        posts:posts
+      Post.find({},function(err,posts){
 
-      });
+        res.render("home", {
+          // passing the key value pair to the ejs file 
+          // passing the global array to the ejs to print that array 
+          startingContent: homeStartingContent,
+          posts:posts
+  
+        });
+
+      })
     });
 
 
@@ -47,40 +69,37 @@ let posts=[];
 
     // the post request we made when we clicks the submit button 
     app.post("/compose",function(req,res){
-      console.log(req.body.postTitle);
+      // console.log(req.body.postTitle);
 
       // creating an Object so that we can store in the global array
-      let post = {
-        title:req.body.postTitle,
-        content:req.body.postBody 
-      };
+      const post = new Post ({
+        title: req.body.postTitle,
+        content: req.body.postBody
+      });
+    
+      // // pushing the object "post" as above to the global array "posts"
+      // posts.push(post);
 
-      // pushing the object "post" as above to the global array "posts"
-      posts.push(post);
+      // instead of sharing in the global variable we store in the databse
+      post.save(function(err){
+        if(!err){
 
-      // redirecting to the home page when user clicks the submit button 
-      res.redirect("/");
+          // redirecting to the home page when user clicks the submit button 
+          res.redirect("/");
+        }
+      });
     });
 
     // making get request for every single article or blog
-    app.get("/posts/:postName",function(req,res){
+    app.get("/posts/:postId",function(req,res){
 
-      // _.lowerCase is a function of lodas and is used to convert all the strings to lovercase 
-      const temp=_.lowerCase(req.params.postName);
+      const requestedPostId = req.params.postId;
 
-      let flag=true
-
-      posts.forEach(function(post){
-
-        if(_.lowerCase(post.title)===temp){
-
-          // sending the post request to home
-          res.render("post",{
-            title:post.title,
-            content:post.content
-
-          });
-        }
+      Post.findOne({_id: requestedPostId}, function(err, post){
+        res.render("post", {
+          title: post.title,
+          content: post.content
+        });
       });
 
     });
